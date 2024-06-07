b619efb8b588546cd9408d8ef1144106cf96b6d6
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-06-08 00:21:57 +0530

    completed js to display daily todo

--- script.js
+++ script.js
@@ -5,6 +5,44 @@ const tempDB = {
     priority: []
 }
 
+const todoList = document.querySelector(".todo-list")
+const listCon = document.querySelector(".list-container")
+
+const checkDB = () => {
+    let todayDate = extractDate()
+    if (localStorage.getItem(todayDate) !== null) {
+
+        let tempObj = JSON.parse(localStorage.getItem(todayDate));
+
+        todoList.classList.remove('hide')
+        let len = tempObj.priority.length
+        for (let i = 0; i < len; i++) {
+            let tempEl = document.createElement("div")
+            tempEl.classList.add("todo-task")
+            tempEl.innerHTML = `<div class="added-priority">${tempObj.priority[i]}</div><div class="added-name">${tempObj.task[i]}</div>`
+
+            listCon.appendChild(tempEl)
+        }
+    }
+    else {
+        start.classList.remove('hide');
+        console.log("HKJ");
+    }
+
+}
+
+const extractDate = () => {
+    const d = new Date()
+
+    const currentDate = d.getDate()
+    const currentMonth = d.getMonth() + 1
+    const currentYear = d.getFullYear()
+
+    return `${currentDate}-${currentMonth}-${currentYear}`
+}
+
+checkDB()
+
 const start = document.querySelector(".start")
 const addToList = document.querySelector(".add-to-list")
 
@@ -49,7 +87,7 @@ plusBtn.addEventListener("click", () => {
 const add = (a, b) => {
     const element = document.createElement("div")
     element.classList.add("added-task")
-    element.innerHTML = `<div class="added-task"><div class="added-priority">${b}</div><div class="added-name">${a}</div></div>`
+    element.innerHTML = `<div class="added-priority">${b}</div><div class="added-name">${a}</div>`
 
     allTasks.appendChild(element)
 }
@@ -57,16 +95,6 @@ const add = (a, b) => {
 const addToObject = (a, b) => {
     tempDB.task.push(a)
     tempDB.priority.push(b)
-}
-
-const extractDate = () => {
-    const d = new Date()
-
-    const currentDate = d.getDate()
-    const currentMonth = d.getMonth() + 1
-    const currentYear = d.getFullYear()
-
-    return `${currentDate}-${currentMonth}-${currentYear}`
 }
 
 addToLocalStorage = () => {
@@ -93,16 +121,8 @@ const viewBtn = document.querySelector(".view-btn")
 
 viewBtn.addEventListener("click", () => {
     successful.classList.add('hide');
+
+    checkDB()
 })
 
-const checkDB = () => {
-    let todayDate = extractDate
-    if (localStorage.getItem(todayDate) !== null) {
-        
-    }
-    else {
-        start.classList.remove('hide');
 
-    }
-
-}
