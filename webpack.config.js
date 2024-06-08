44ef93b29bcddbf07aaa3919768cbd5497e3238e
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-06-08 08:54:36 +0530

    added webpack starter files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+
+    entry: './src/index.js',
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Daily TODO"
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
