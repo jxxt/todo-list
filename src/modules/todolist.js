b49188cd8185c9ad108d3e6a0b0d33dcb339f445
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-06-08 17:53:06 +0530

    added todolist.js

--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -4,7 +4,7 @@ export default function todolist() {
     const todoListElement = document.createElement("div")
     todoListElement.classList.add("todo-list")
     todoListElement.classList.add("hide")
-    todoListElement.innerHTML= ``
+    todoListElement.innerHTML = `<div class="lline"><div class="lline1">your today's</div><div class="lline1"><u>schedule</u></div></div><div class="list-container"></div>`
 
     container.appendChild(todoListElement)
 };
